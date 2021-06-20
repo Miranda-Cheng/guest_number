917dfebfeb5a7ccea8f99a139655fba86a327c71
Miranda Cheng <t05230520@gmail.com>
2021-06-20 22:51:12 +0800

    add count

--- guest_number.py
+++ guest_number.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1 , 100)
+count = 0
 while True:
+	count += 1   # count = count + 1的快寫法
 	a = input('請猜數字:')
 	a = int(a)
 	if a < r:
@@ -9,4 +11,6 @@ while True:
 		print('比答案大')
 	else:
 		print('恭喜你 ~ 終於猜對了!!!')
+		print('這是你猜的第' ,count , '次')
 		break
+	print('這是你猜的第' ,count , '次')
